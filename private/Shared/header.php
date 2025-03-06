156b4fda7cdc610819ca5d5793de50bac5991374
May <19104@rosmini.school.nz>
2025-03-07 11:23:04 +1300

    write a message

--- private/Shared/header.php
+++ private/Shared/header.php
@@ -1,11 +1,28 @@
 <!DOCTYPE html>
+<link rel="stylesheet" href="<?php echo("/public/stylesheet/styles.css");?>">
 <html lang="en">
+<header> 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
-    <title>Document</title>
+  <meta charset="UTF-8" />
+  <title>Community issue</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  <meta name="viewport" content="width=device-width,initial-scale=1" />
+  <meta name="description"/>
 </head>
-<body>
-    
+<body>	
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+	
+	<img src="<?php echo("")?>">
+	
+<!-- Navigation Bar -->
+<div>
+	<nav class="Navbar">
+  		<a href="">home</a>
+ 	 	<a href="">The Issue</a>
+  		<a href="">affect</a>
+		<a href="">Solution</a>
+	</nav>
+</div>
 </body>
+</header>
 </html>
