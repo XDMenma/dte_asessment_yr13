32a1267270ed696e98a169993c1b28b9c83b8092
May <19104@rosmini.school.nz>
2025-03-13 15:04:55 +1300

    write a message

--- private/Shared/footer.php
+++ private/Shared/footer.php
@@ -2,8 +2,20 @@
 <html lang="en">
 <link rel="stylesheet" href="<?php echo("../../../public/stylesheet/styles.css");?>">
 <footer>
-
-
+<h3>Contact Us</h3>
+	<h3>36 Dominion Street</h3>
+	<h3>Takapuna</h3>
+	<h3>Auckland, 0622</h3>
+	<h3>Phone: +64 9 489 5417</h3>
+	<h3>admin@rosmini.school.nz</h3>
+<div>
+	<nav class="Navbar">
+  		<a href="">home</a>
+ 	 	<a href="">The Issue</a>
+  		<a href="">affect</a>
+		<a href="">Solution</a>
+	</nav>
+</div>
 
 </footer>
 </html>
--- public/Home.php
+++ public/Home.php
@@ -2,3 +2,32 @@
 <?php require_once("../private/initialize.php")?>
 <?php include (SHARED_PATH . "/header.php")?>
 <h1>HOME PAGE</h1>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php include (SHARED_PATH . "/footer.php") ?>
