64515cb7c5a9b4d1cc40ad60cdbbcf51f3165a6b
May <19104@rosmini.school.nz>
2025-03-13 15:06:40 +1300

    write a message

--- private/Shared/footer.php
+++ private/Shared/footer.php
@@ -2,12 +2,12 @@
 <html lang="en">
 <link rel="stylesheet" href="<?php echo("../../../public/stylesheet/styles.css");?>">
 <footer>
-<h3>Contact Us</h3>
+<h3>Contact Me</h3>
 	<h3>36 Dominion Street</h3>
 	<h3>Takapuna</h3>
 	<h3>Auckland, 0622</h3>
-	<h3>Phone: +64 9 489 5417</h3>
-	<h3>admin@rosmini.school.nz</h3>
+	<h3>Phone: +64 123 456 7890</h3>
+	<h3>19104@rosmini.school.nz</h3>
 <div>
 	<nav class="Navbar">
   		<a href="">home</a>
