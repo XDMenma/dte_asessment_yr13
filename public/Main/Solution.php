62d729c62460caa09764f668a486c20f3e8c30bc
May <19104@rosmini.school.nz>
2025-03-21 11:20:05 +1300

    write a message

--- public/Main/Solution.php
+++ public/Main/Solution.php
@@ -1,3 +1,35 @@
 <link rel="stylesheet" href="<?php echo("../stylesheet/styles.css");?>">
 <?php require_once("../../private/initialize.php")?>
 <?php include (SHARED_PATH . "/header.php")?>
+<h1>What can we do to help</h1>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php include (SHARED_PATH . "/footer.php") ?>
--- public/Main/affect.php
+++ public/Main/affect.php
@@ -1,3 +1,34 @@
 <link rel="stylesheet" href="<?php echo("../stylesheet/styles.css");?>">
 <?php require_once("../../private/initialize.php")?>
 <?php include (SHARED_PATH . "/header.php")?>
+<h1> the affect is has on people</h1>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php include (SHARED_PATH . "/footer.php") ?>
