1b1d0666561cb0ec4b7fb2cd14cbadc2f83039c8
May <19104@rosmini.school.nz>
2025-03-24 11:28:16 +1300

    write a message

--- public/Home.php
+++ public/Home.php
@@ -2,32 +2,45 @@
 <?php require_once("../private/initialize.php")?>
 <?php include (SHARED_PATH . "/header.php")?>
 <h1>HOME PAGE</h1>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+<div id="carouselExampleCaptions" class="carousel slide">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+	<button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="3" aria-label="Slide 4"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="4" aria-label="Slide 5"></button>
+  </div>
+<div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="<?php echo("https://www.barfoot.co.nz/media/w5onnctc/auckland-house-styles-3.jpg")?>" class="d-block w-100">
+      <div class="carousel-caption d-none d-md-block">
+      </div>
+    </div>
+<div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
+    <div class="carousel-item">
+      <img src="<?php echo("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpheD-BKJxEH3xPRKDc4lR74nItBvQmx9EKw&s")?>" class="d-block w-100">
+      <div class="carousel-caption d-none d-md-block">
+      </div>
+	</div>
+</div>
+<div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
+    <div class="carousel-item">
+      <img src="<?php echo("ihttps://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYRpiz8Cz-LUEHV9q1iupQxSWE9UTAymDpVw&s")?>" class="d-block w-100">
+      <div class="carousel-caption d-none d-md-block">
+	  </div>
+	</div>
+</div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+<h2>Global House pricing</h2>
+<p>Housing is a critical issue that affects communities globally, impacting not only the individuals and families directly affected but also the broader social and economic fabric of neighborhoods. With rising property prices and skyrocketing rental costs, combined with limited affordable housing options, many communities around the world are grappling with a housing crisis that deepens inequality and hampers access to basic necessities. The steep cost of housing often forces individuals and families to allocate a disproportionate amount of their income toward rent or mortgages, leading to financial instability. This, in turn, can result in displacement, overcrowding, and increased pressure on social services, which affects education, health, and overall quality of life. Addressing the housing problem, including the challenge of inflated housing prices, is essential for creating sustainable, equitable communities globally, where everyone has the opportunity to thrive.</p>
 <?php include (SHARED_PATH . "/footer.php") ?>
